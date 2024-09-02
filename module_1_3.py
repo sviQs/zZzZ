11aea6019d529d0de8dfbe99d56349d2251b8ba6
Alexander <alexaander.yudin@gmail.com>
2024-09-02 16:00:40 +0500

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,8 +2,8 @@ Name = 'Sasha'
 print ('Name:',Name)
 Age = 23
 print ('Age:',Age)
-New_age = 1
-print ('New Age:',Age + New_age)
+New_age = (Age + 1)
+print ('New Age:',New_age)
 Is_Student = True
 print ('Is Student:',Is_Student)
 
